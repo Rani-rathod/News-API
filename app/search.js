82ebc54dbdbdcda2a6429a1287ac7b11d6ca0ea4
Rani <ranirathor20@navgurukul.org>
2024-03-27 14:50:24 +0530

    Working on Search

--- app/search.js
+++ app/search.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const Search=()=>{
     return (
-        <div className='searchText'>
+        <div className='contener'>
             <form class="d-flex justify-content-center mt-3 mt-md-0 " role="search">
                 <input class="form mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
                 <button class="btn btn-danger" type="submit">Go</button>
